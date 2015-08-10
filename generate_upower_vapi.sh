1835f9eba9f9b2db24d84f1a9afc9c46e13a3402
Eugenio Paolantonio (g7) <me@medesimo.eu>
2015-08-10 16:50:46 +0200

    [Recipe] Use directly the .gir bindings instead of a manually-generated vapi for UPowerGlib-1.0. Fixes #2.
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- generate_upower_vapi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# This is so ugly that I do not even put here a license.
-# Or my name.
-
-cd vapis
-vapigen --pkg gio-2.0 --library UPowerGlib-1.0 /usr/share/gir-1.0/UPowerGlib-1.0.gir
-
-mv UPowerGlib-1.0.vapi upower-glib.vapi
-sed 's/UPowerGlib-1.0.h/upower.h/g' -i upower-glib.vapi
